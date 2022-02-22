41bc833bc9c462cb0bfec613e41f618c7fdcd63b
amir69prog <amirsacum@gmail.com>
2022-02-22 18:15:30 +0330

    Init: Permission IsUserVerified

--- api/users/permissions.py
+++ api/users/permissions.py
@@ -1,11 +1,21 @@
 from rest_framework import permissions
+from .messages import ViewMessages
+
 
 class IsSelfOrReadOnly(permissions.BasePermission):
     """
         Permission for user_detail view to check the user is the current user
     """
+
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
-            return True # ReadOnly
-        
+            return True  # ReadOnly
+
         return obj == request.user
+
+
+class IsUserVerified(permissions.BasePermission):
+    message = ViewMessages.USER_IS_NOT_VERIFIED.value
+
+    def has_permission(self, request, view):
+        return request.user.is_verified
