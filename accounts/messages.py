fd2e112d2065b0ea6a39dd239c51e3ed9370b4ba
amir69prog <amirsacum@gmail.com>
2022-02-27 15:42:13 +0330

    Init: Messages for customusers

--- accounts/messages.py
+++ accounts/messages.py
@@ -4,3 +4,5 @@ from enum import Enum
 class UserMessage(Enum):
     PHONE_NUMBER_HELP_TEXT = 'Please enter your phone number as a following template\n\
 use one of these: [09120000000 | +9891200000000 | 00989120000000]'
+    PROFILE_UPDATED_SUCCESSFULLY = 'Your account has been updated successfully.'
+    
