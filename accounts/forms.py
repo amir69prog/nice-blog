0fbe6b46b50edfb7a6bac13a12b4b5b4971d92f6
amir69prog <amirsacum@gmail.com>
2022-02-27 15:47:07 +0330

    Init: UserProfileForm initialized

--- accounts/forms.py
+++ accounts/forms.py
@@ -13,4 +13,10 @@ class CustomUserCreationForm(UserCreationForm):
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = ('phone_number', 'email', 'username')
+        fields = ('phone_number', 'email', 'username',) 
+
+
+class UserProfileForm(forms.ModelForm):
+     class Meta:
+        model = CustomUser
+        fields = ('phone_number', 'email', 'username', 'first_name', 'last_name') 
