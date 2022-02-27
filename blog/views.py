ff84d8ddfa56bb84b9884e16c4f40fe76c885900
amir69prog <amirsacum@gmail.com>
2022-02-27 15:51:15 +0330

    Init: blog views `CRUD` process of posts

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,74 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib import messages
 
 from .models import Post
+from .mixins import UserVerifiedPermission, IsAuthorAndVerifiedPermission
+from .messages import PostMessages 
 
-class PostListView(ListView):
+
+class PostListView(LoginRequiredMixin,
+                    UserVerifiedPermission, ListView):
+    login_url = '/accounts/login/'
     model = Post
-    template_name = 'blog/post_list.html'
+    template_name = 'blog/index.html'
+    paginate_by = 5
 
 
-class PostDetailView(DetailView):
+class PostDetailView(LoginRequiredMixin,
+                    UserVerifiedPermission, DetailView):
+    login_url = '/accounts/login/'
     model = Post
-    template_name = 'blog/post_detail.html'
+    template_name = 'blog/post.html'
+
+
+class CreatePostView(LoginRequiredMixin,
+                    UserVerifiedPermission, CreateView):
+    login_url = '/accounts/login/'
+    model = Post
+    fields = ('title', 'body', 'reading_time')
+    template_name = 'blog/create_post.html'
+
+    def form_valid(self, form):
+        """ When submitting form if that valid author will set for this post """
+        form.instance.author = self.request.user
+        messages.add_message(self.request, messages.SUCCESS, PostMessages.POST_CREATED_SUCCESSFULLY.value)
+        return super().form_valid(form)
+    
+    def get_success_url(self) -> str:
+        return reverse('accounts:profile')
+
+
+class DeletePostView(LoginRequiredMixin,
+                    IsAuthorAndVerifiedPermission,
+                    DeleteView):
+    login_url = '/accounts/login/' 
+    model = Post
+    template_name = 'blog/delete_post.html'
+
+    def form_valid(self, form):
+        """ When submitting form if that valid author will set for this post """
+        messages.add_message(self.request, messages.SUCCESS, PostMessages.POST_DELETED_SUCCESSFULLY.value)
+        return super().form_valid(form)
+
+    def get_success_url(self) -> str:
+        return reverse('accounts:profile')
+
+
+class UpdatePostView(LoginRequiredMixin,
+                    IsAuthorAndVerifiedPermission,
+                    UpdateView):
+    login_url = '/accounts/login/'
+    model = Post
+    fields = ('title', 'body', 'reading_time')
+    template_name = 'blog/update_post.html'
+
+    def form_valid(self, form):
+        """ When submitting form if that valid adding message """
+        messages.add_message(self.request, messages.SUCCESS, PostMessages.POST_UPDATED_SUCCESSFULLY.value)
+        return super().form_valid(form)
+    
+    def get_success_url(self) -> str:
+        return reverse('accounts:profile')
