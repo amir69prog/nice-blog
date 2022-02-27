42d613dab7299779dbfcdb2895f9bbfde02e9032
amir69prog <amirsacum@gmail.com>
2022-02-27 15:50:30 +0330

    Init: URLs for blog app

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path, re_path
+from django.urls import path
 
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, CreatePostView, DeletePostView, UpdatePostView
 
 
 app_name = 'blog'
-
 urlpatterns = [
     path('', PostListView.as_view(), name='list'),
-    path('<int:pk>/', PostDetailView.as_view(), name='detail'),
+    path('<int:pk>/<str:slug>/', PostDetailView.as_view(), name='detail'),
+    path('add/', CreatePostView.as_view(), name='add'),
+    path('<int:pk>/<str:slug>/delete/', DeletePostView.as_view(), name='delete'),
+    path('<int:pk>/<str:slug>/edit/', UpdatePostView.as_view(), name='edit'),
 ]
