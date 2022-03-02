ff062dfdd5778a625ee1ff50b90f330858fc478f
amir69prog <amirsacum@gmail.com>
2022-03-02 22:41:39 +0330

    Init: Documenting

--- config/urls.py
+++ config/urls.py
@@ -3,9 +3,25 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework import permissions 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+)
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Blog API',
+        default_version='v1',
+        description="A sample API for learning DRF",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="hello@example.com"),
+        license=openapi.License(name="BSD License"),
+        ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 
@@ -23,6 +39,15 @@ urlpatterns = [
     
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # Documenting
+    path('swagger/', schema_view.with_ui(
+        'swagger', cache_timeout=0
+    ), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui(
+        'redoc', cache_timeout=0
+    ), name='schema-redoc'),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
